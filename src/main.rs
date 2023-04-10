c5a7c1ba0865b742d71651eba276da2caf23c656
Earl Pomeroy <earl.pomeroy42@gmail.com>
2023-04-09 18:14:11 -0600

    MVK - Minimum viable kernel
    Post-02

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link to the Rust standard lib
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
+// Function to call on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
